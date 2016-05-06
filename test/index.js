e6287e065643620d4e1f7fb7238d42b1c7e53363
Jeff Escalante <hello@jenius.me>
2016-05-06 17:36:35 -0400

    options test

--- test/index.js
+++ test/index.js
@@ -3,12 +3,6 @@ require('dotenv').config({ silent: true })
 const test = require('ava')
 const Rooftop = require('..')
 
-const api = new Rooftop({
-  name: process.env.name,
-  apiToken: process.env.token,
-  contentTypes: ['posts', 'case_studies']
-})
-
 test('errors without a "name"', (t) => {
   t.throws(
     () => { new Rooftop() }, // eslint-disable-line
@@ -29,9 +23,36 @@ test('initializes with a name and apiToken', (t) => {
 })
 
 test('returns valid content', (t) => {
+  const api = new Rooftop({
+    name: process.env.name,
+    apiToken: process.env.token,
+    contentTypes: ['posts', 'case_studies']
+  })
+
   return api.run({ options: { locals: {} } }, undefined, () => {})
     .then((res) => {
       t.is(res.posts.length, 2)
       t.is(res.case_studies.length, 1)
     })
 })
+
+test('implements request options', (t) => {
+  const api = new Rooftop({
+    name: process.env.name,
+    apiToken: process.env.token,
+    contentTypes: [{
+      name: 'posts',
+      search: 'hello'
+    }]
+  })
+
+  return api.run({ options: { locals: {} } }, undefined, () => {})
+    .then((res) => {
+      t.is(res.posts.length, 1)
+      t.is(res.posts[0].slug, 'testing-123')
+    })
+})
+
+test.todo('implements transform function')
+test.todo('can disable transform function')
+test.todo('works with custom transform function')
