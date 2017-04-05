1973767f0109f0fb8fdd0841e705af1db9740b21
Jeff Escalante <escalantejeff@gmail.com>
2017-04-05 12:58:50 -0400

    wip upgrade, issue with rooftop client proxy

--- test/fixtures/default/app.js
+++ test/fixtures/default/app.js
@@ -1,13 +1,7 @@
 const Rooftop = require('../../..')
-const htmlStandards = require('spike-html-standards')
+const htmlStandards = require('reshape-standard')
 const locals = {}
 
 module.exports = {
-  matchers: { html: '*(**/)*.sgr' },
-  reshape: (ctx) => htmlStandards({ webpack: ctx, locals }),
-  plugins: [new Rooftop({
-    url: process.env.url,
-    apiToken: process.env.token,
-    addDataTo: locals
-  })]
+  matchers: { html: '*(**/)*.sgr' }
 }
